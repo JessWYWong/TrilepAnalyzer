1dd2e724fb4c52bceb686f1a36ead00a133717fa
Rizki Syarif <rizki_syarif@brown.edu>
2016-09-21 17:21:47 -0400

    delete files

--- makeKinematics/switchToWithTT.sh
+++
@@ -1,5 +0,0 @@
-cp -v analyze.py_now analyze.py
-cp -v doHists.py_withTT doHists.py
-cp -v doKinematics.py_withTT doKinematics.py
-cp -v plotKinematics.py_withTT plotKinematics.py
-cp -v samples.py_now samples.py
