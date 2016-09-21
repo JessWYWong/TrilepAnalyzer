3a7799f0135bfb6deeda41e726c067fe0e22da6c
Rizki Syarif <rizki_syarif@brown.edu>
2016-09-21 17:22:29 -0400

    noDDBKG version

---
+++ makeKinematics/switchToNoDDBKG.sh
@@ -0,0 +1,5 @@
+cp -v analyze.py_now analyze.py
+cp -v doHists.py_noDDBKG doHists.py
+cp -v doKinematics.py_noDDBKG doKinematics.py
+cp -v plotKinematics.py_noDDBKG plotKinematics.py
+cp -v samples.py_now samples.py
