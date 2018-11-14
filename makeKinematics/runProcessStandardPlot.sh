575577384686d63edb6df40b3f7aaf015278f380
Rizki Syarif <rizki_syarif@brown.edu>
2018-11-14 11:24:49 -0500

    1st working version for plotter for 2017data, part 2

---
+++ makeKinematics/runProcessStandardPlot.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+# #######################################################################################################
+# 1st attempt dor 2017data
+# day=2018_11_12
+
+# source switchToFinal.sh  
+# 
+# #####doKinematics.py <folder> <whichPlots?>
+# python -u doKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_FRCR2_$day 1
+# ###plotKinematics.py <folder> <blind?> <log?>
+# python -u plotKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_FRCR2_$day 0 0
+# python -u plotKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_FRCR2_$day 0 1
+# 
+# #####doKinematics.py <folder> <whichPlots?>
+# python -u doKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_SR_$day 1
+# ###plotKinematics.py <folder> <blind?> <log?>
+# python -u plotKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_SR_$day 0 0
+# python -u plotKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_SR_$day 0 1
+# # 
+# # #####doKinematics.py <folder> <whichPlots?>
+# python -u doKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_CR2_$day 1
+# # ###plotKinematics.py <folder> <blind?> <log?>
+# python -u plotKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_CR2_$day 0 0
+# python -u plotKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_CR2_$day 0 1
+# # 
+
+
+# #######################################################################################################
+# 2nd attempt dor 2017data
+day=2018_11_13
+
+# #####doKinematics.py <folder> <whichPlots?>
+# python -u doKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_SR_$day 1
+# ###plotKinematics.py <folder> <blind?> <log?>
+# python -u plotKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_SR_$day 0 0
+# python -u plotKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_SR_$day 0 1
+
+
+# #####doKinematics.py <folder> <whichPlots?>
+python -u doKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_CR2_$day 1
+# ###plotKinematics.py <folder> <blind?> <log?>
+python -u plotKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_CR2_$day 0 0
+python -u plotKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs_CR2_$day 0 1
+
+
+#######################################################################################################
+
+####REMEMBER to CHECK step1Dir in doHists before submitting!#############################
+##checking MCBKg Gamma samples
+# 
+# day=2017_12_1
+# source switchToNoDDBKGfinal.sh  
+# 
+# python -u doKinematics.py kinematics_LJMet80x_extra_wGammas_noDDBKG_SR_$day 0
+
---
+++ makeKinematics/submitCondors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+###REMEMBER to CHECK step1Dir in doHists AND analyze.py_final before submitting!#############################
+# source switchToFinal.sh  
+# python doCondorKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs FRCR2
+# python doCondorKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs SR
+python doCondorKinematics.py kinematics_LJMet94x_3lepTT_2017datasets_2018_11_7_rizki_NoSYS_2016SFs CR2
+
+
+####REMEMBER to CHECK step1Dir in doHists before submitting!#############################
+# source switchToNoDDBKGfinal.sh  
+# 
+# python doCondorKinematics.py kinematics_LJMet80x_extra_wGammas_noDDBKG SR
